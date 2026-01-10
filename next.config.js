3594442cbe3e0289d6306684b675441c8d7b46d9
Rohan Gupta <rohanyashraj@gmail.com>
2026-01-10 19:12:42 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '192.168.29.29'],
+}
