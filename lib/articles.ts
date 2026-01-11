80f45a3f99c809cfefb343491d49727e0b5fca74
Rohan Gupta <rohanyashraj@gmail.com>
2026-01-11 19:20:16 +0530

    reordered blogs based on date

--- lib/articles.ts
+++ lib/articles.ts
@@ -34,9 +34,9 @@ export const getSortedArticles = (): ArticleItem[] => {
     const dateOne = moment(a.date, format)
     const dateTwo = moment(b.date, format)
     if (dateOne.isBefore(dateTwo)) {
-      return -1
-    } else if (dateTwo.isAfter(dateOne)) {
       return 1
+    } else if (dateOne.isAfter(dateTwo)) {
+      return -1
     } else {
       return 0
     }
