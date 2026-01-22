585e092e25fdb33a83cfae446391c4dfc0d14dee
Rohan Gupta <rohanyashraj@gmail.com>
2026-01-23 01:13:02 +0530

    added MS Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -147,6 +147,13 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <script type="text/javascript">
+           (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v5kzs8yv6p");
+        </script>
       </head>
       <body
         className={`${cormorantGaramond.variable} ${outfit.variable} bg-white text-zinc-900 antialiased selection:bg-zinc-900 selection:text-white flex flex-col min-h-screen`}
